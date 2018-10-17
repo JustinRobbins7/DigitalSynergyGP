fb050971554e6d94361a685bff1afcaf5357d0e6
tconnole <tcconnole728@gmail.com>
2018-10-17 08:23:10 -0500

    added documentation to app.py

--- WebApp/WebApp/app.py
+++ WebApp/WebApp/app.py
@@ -1,38 +1,11 @@
-# from flask import Flask
-# import flask
-#
-# app = Flask(__name__)
-#
-#
-# @app.route('/')
-# def index():
-#     return flask.render_template("index.html")
-#
-#
-# @app.route('/about.html/')
-# def about():
-#     return flask.render_template("about.html")
-#
-#
-# @app.route('/gallery.html/')
-# def gallery():
-#     return flask.render_template("gallery.html")
-#
-#
-# @app.route('/menu.html/')
-# def menu():
-#     return flask.render_template("menu.html")
-#
-#
-# @app.route('/myaccount.html/')
-# def myaccount():
-#     return flask.render_template("myaccount.html")
+
 
 from flask import Flask, render_template, abort
 from jinja2 import TemplateNotFound
 app = Flask(__name__)
 
 
+# application will load html based on URL route given in the browser
 @app.route('/', defaults={'page': 'home.html'})
 @app.route('/<page>')
 def html_lookup(page):
@@ -43,5 +16,6 @@ def html_lookup(page):
         abort(404)
 
 
+# runs application
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
