079a4df4bff54c609d6bc1975739388ccf4c9feb
caelanbryan <cpbryan@unomaha.edu>
2018-11-04 13:17:19 -0600

    added forms and tested form on myaccount page

--- WebApp/WebApp/app.py
+++ WebApp/WebApp/app.py
@@ -2,7 +2,11 @@
 
 from flask import Flask, render_template, abort
 from jinja2 import TemplateNotFound
+from WebApp.WebApp.forms import LoginForm, AddMenuItem, CreateAccount
 app = Flask(__name__)
+
+# secret key to allow for CSRF forms
+app.config['SECRET_KEY'] = 'any secret string'
 
 
 # application will load html based on URL route given in the browser
@@ -11,10 +15,20 @@ app = Flask(__name__)
 def html_lookup(page):
     renderpage = page + '.html'
     print(page)
-    try:
-        return render_template('{}'.format(renderpage))
-    except TemplateNotFound:
-        abort(404)
+    # has to load page differently with forms, check for myaccount before the general render
+    if page == 'myaccount':
+        try:
+            form_login = LoginForm()
+            form_menu = AddMenuItem()
+            form_create = CreateAccount()
+            return render_template('myaccount.html', formLogin=form_login, formMenu=form_menu, formCreate=form_create)
+        except TemplateNotFound:
+            abort(404)
+    else:
+        try:
+            return render_template('{}'.format(renderpage))
+        except TemplateNotFound:
+            abort(404)
 
 
 # runs application
--- WebApp/WebApp/forms.py
+++ WebApp/WebApp/forms.py
@@ -1,15 +1,27 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, DecimalField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, equal_to
 
 
+# form for creating account
+class CreateAccount(FlaskForm):
+    username = StringField('Please Enter a Username', validators=[DataRequired()])
+    password = PasswordField('Please Enter a Password', validators=[DataRequired()])
+    # validate for same password as above
+    passwordverify = PasswordField('Please Re-Enter Your Password', validators=[equal_to(password), DataRequired()])
+    createaccount = SubmitField('Create Account')
+
+
+# form for logging into the system
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     loginsubmit = SubmitField('Sign In')
 
 
+# form for adding a menu item
 class AddMenuItem(FlaskForm):
     title = StringField('Menu Item', validators=[DataRequired()])
     description = StringField('Description', validators=[DataRequired()])
     price = DecimalField('Price', places=2, rounding=None, validators=[DataRequired()])
+    addsubmit = SubmitField('Add Item')
